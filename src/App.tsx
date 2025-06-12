fff994070abde13dde27a58c6f9ffba7bc28b27e
Horquidia <horquidia.a.lima@gmail.com>
2025-06-12 14:43:20 -0300

    Deixando Header Dinâmica

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import styles from "./app.module.css"
 import { Header } from "./components/Header"
 
 export default function App() {
+    function handleRestartGame(){
+        alert("Recomeçar o jogo")
+    }
+
     return (
         <div className={styles.container}>
             <main>
-                <Header />
+                <Header current={5} max={10} onRestart={handleRestartGame}/>
             </main>  
         </div>
     )
