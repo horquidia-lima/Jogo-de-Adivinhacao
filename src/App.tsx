3cd2c1c5ba364bf8af6a190e82c3bdad2e99fb26
Horquidia <horquidia.a.lima@gmail.com>
2025-06-14 20:14:00 -0300

    Criando Componente de Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styles from "./app.module.css"
 import { Header } from "./components/Header"
+import { Input } from "./components/Input"
 import { Letter } from "./components/Letter"
 import { Tip } from "./components/Tip"
 
@@ -19,6 +20,10 @@ export default function App() {
                     <Letter value="A"/>
                     <Letter value="C"/>
                     <Letter value="T"/>
+                </div>
+                <h4>Palpite</h4>
+                <div>
+                    <Input autoFocus maxLength={1} placeholder="?"/>
                 </div>
             </main>  
         </div>
